9d362f3077f0a06cc5884a6f83d2d4d2e9eb0b9d
jinukeu <koownij@kakao.com>
2023-06-21 17:39:36 +0900

    chore: Outcome 제네릭 타입 수정

--- domain/src/main/java/com/onewx2m/domain/Outcome.kt
+++ domain/src/main/java/com/onewx2m/domain/Outcome.kt
@@ -1,6 +1,6 @@
 package com.onewx2m.domain
 
-sealed class Outcome<T> {
+sealed class Outcome<out T> {
     object Loading : Outcome<Nothing>()
     data class Success<T>(val data: T) : Outcome<T>()
     data class Failure<T>(val error: Throwable?) : Outcome<T>()
