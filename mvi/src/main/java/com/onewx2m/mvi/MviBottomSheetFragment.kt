e90f0ddd900bf553bb06e8dd3f37e69da38c89cb
jinukeu <koownij@kakao.com>
2023-06-24 17:09:09 +0900

    feat: MviBottomSheetFragment 생성

---
+++ mvi/src/main/java/com/onewx2m/mvi/MviBottomSheetFragment.kt
@@ -0,0 +1,79 @@
+package com.onewx2m.mvi
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+abstract class MviBottomSheetFragment<B : ViewDataBinding, STATE : ViewState, EVENT : Event, SIDE_EFFECT: SideEffect ,VM : MviViewModel<STATE, EVENT, SIDE_EFFECT>>(
+    private val inflater: (LayoutInflater, ViewGroup?, Boolean) -> B,
+) : Fragment() {
+    private var _binding: B? = null
+    protected val binding
+        get() = _binding!!
+
+    protected abstract val viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = inflater(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        initView()
+        observeState(::render, ::handleSideEffect)
+    }
+
+    protected abstract fun initView()
+
+    private fun observeState(render: (STATE) -> Unit, handleSideEffect: (SIDE_EFFECT) -> Unit) {
+        repeatOnStarted(viewLifecycleOwner) {
+            viewModel.state.onEach {
+                render(it)
+            }.launchIn(this)
+
+            viewModel.sideEffects.onEach {
+                handleSideEffect(it)
+            }.launchIn(this)
+        }
+    }
+
+    protected open fun render(current: STATE) {
+
+    }
+
+    protected open fun handleSideEffect(sideEffect: SIDE_EFFECT) {
+
+    }
+
+    protected fun LifecycleOwner.repeatOnStarted(viewLifecycleOwner: LifecycleOwner, block: suspend CoroutineScope.() -> Unit) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED, block)
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
