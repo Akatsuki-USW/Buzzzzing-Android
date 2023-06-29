f74bfa74925e37c0d1aeeb94f360ebd903141fe0
jinukeu <koownij@kakao.com>
2023-06-29 18:36:50 +0900

    feat: 메인 버튼 컴포넌트 생성

---
+++ design-system/src/main/java/com/onewx2m/design_system/components/button/MainButton.kt
@@ -0,0 +1,98 @@
+package com.onewx2m.design_system.components.button
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
+import androidx.databinding.DataBindingUtil
+import com.onewx2m.core_ui.extensions.onThrottleClick
+import com.onewx2m.design_system.R
+import com.onewx2m.design_system.databinding.ButtonMainBinding
+
+sealed interface MainButtonState {
+    object Positive : MainButtonState
+    object Negative : MainButtonState
+    object Loading : MainButtonState
+    object Disable : MainButtonState
+}
+
+class MainButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) :
+    FrameLayout(context, attrs, defStyleAttr) {
+
+    fun onThrottleClick(onClick: () -> Unit) =
+        binding.constraintLayoutButton.onThrottleClick {
+            onClick()
+        }
+
+    private val binding: ButtonMainBinding
+
+    var state: MainButtonState = MainButtonState.Positive
+        set(value) {
+            field = value
+            when (field) {
+                MainButtonState.Positive -> changeStateToPositive()
+                MainButtonState.Negative -> changeStateToNegative()
+                MainButtonState.Loading -> changeStateToLoading()
+                MainButtonState.Disable -> changeStateToDisable()
+            }
+        }
+
+    private fun changeStateToPositive() {
+        setButtonClickFocus(true)
+        binding.apply {
+            constraintLayoutButton.setBackgroundResource(R.drawable.ripple_gray01_bg_solid_blue_rounded_5)
+            textViewContent.setTextColor(context.getColor(R.color.white01))
+        }
+        hideLoading()
+    }
+
+    private fun changeStateToNegative() {
+        setButtonClickFocus(true)
+        binding.apply {
+            constraintLayoutButton.setBackgroundResource(R.drawable.ripple_gray01_bg_solid_blue_light_rounded_5)
+            textViewContent.setTextColor(context.getColor(R.color.white01))
+        }
+        hideLoading()
+    }
+
+    private fun changeStateToLoading() {
+        setButtonClickFocus(false)
+        binding.apply {
+            progressBarLoadingButton.visibility = View.VISIBLE
+            viewLoading.visibility = View.VISIBLE
+        }
+    }
+
+    private fun changeStateToDisable() {
+        setButtonClickFocus(false)
+        binding.apply {
+            constraintLayoutButton.setBackgroundResource(R.drawable.bg_solid_gray06_rounded_5)
+            textViewContent.setTextColor(context.getColor(R.color.gray03))
+        }
+        hideLoading()
+    }
+
+    private fun setButtonClickFocus(state: Boolean) {
+        binding.constraintLayoutButton.apply {
+            isClickable = state
+            isFocusable = state
+        }
+    }
+
+    private fun hideLoading() {
+        binding.apply {
+            progressBarLoadingButton.visibility = View.INVISIBLE
+            viewLoading.visibility = View.GONE
+        }
+    }
+
+    init {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        binding = DataBindingUtil.inflate(inflater, R.layout.button_main, this, true)
+    }
+}
