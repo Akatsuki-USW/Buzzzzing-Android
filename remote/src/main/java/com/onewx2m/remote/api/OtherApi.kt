adf34b6edd10f0f825cddbda4cca6fda049e0ae7
jinukeu <koownij@kakao.com>
2023-07-04 16:04:10 +0900

    fix: 닉네임 검증 API Http Method POST -> GET으로 변경

--- remote/src/main/java/com/onewx2m/remote/api/OtherApi.kt
+++ remote/src/main/java/com/onewx2m/remote/api/OtherApi.kt
@@ -2,9 +2,8 @@ package com.onewx2m.remote.api
 
 import com.onewx2m.remote.ApiResult
 import com.onewx2m.remote.model.ApiResponse
-import com.onewx2m.remote.model.response.JwtResponse
 import com.onewx2m.remote.model.response.VerifyNicknameResponse
-import retrofit2.http.POST
+import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface OtherApi {
@@ -12,7 +11,7 @@ interface OtherApi {
         private const val USER = "users"
     }
 
-    @POST("$USER/check/nickname/{nickname}")
+    @GET("$USER/check/nickname/{nickname}")
     suspend fun verifyNickname(
         @Path("nickname") request: String,
     ): ApiResult<ApiResponse<VerifyNicknameResponse>>
