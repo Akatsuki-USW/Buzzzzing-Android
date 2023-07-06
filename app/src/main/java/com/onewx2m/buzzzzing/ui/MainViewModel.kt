4e430152ce711256c4e4ce56faa757ec30d9c73e
jinukeu <koownij@kakao.com>
2023-07-06 15:15:02 +0900

    feat: 로그인에서 메인으로 바로 넘어가는 경우 스플래시 화면이 바로 사라지지 않게 딜레이 추가

--- app/src/main/java/com/onewx2m/buzzzzing/ui/MainViewModel.kt
+++ app/src/main/java/com/onewx2m/buzzzzing/ui/MainViewModel.kt
@@ -1,44 +1,47 @@
 package com.onewx2m.buzzzzing.ui
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.onewx2m.domain.Outcome
 import com.onewx2m.domain.usecase.ReissueJwtUseCase
-import com.onewx2m.mvi.Event
 import com.onewx2m.mvi.MviViewModel
-import com.onewx2m.mvi.SideEffect
-import com.onewx2m.mvi.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
-
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val reissueJwtUseCase: ReissueJwtUseCase
+    private val reissueJwtUseCase: ReissueJwtUseCase,
 ) : MviViewModel<MainViewState, MainEvent, MainSideEffect>(MainViewState()) {
 
     var preDrawRemoveFlag = false
+    private val hideSplashScreenDelay = 250L // 로그인에서 메인으로 넘어가기 전에 스플래시 화면이 사라지므로 약간의 딜레이를 줌.
 
     fun reissueJwtAndNavigateFragmentAndHideSplashScreen() = viewModelScope.launch {
         reissueJwtUseCase().collect { outcome ->
-            when(outcome) {
-                is Outcome.Loading -> {  }
-                is Outcome.Success -> { postSideEffect(MainSideEffect.GoToHomeFragment) }
-                is Outcome.Failure -> {  }
+            when (outcome) {
+                is Outcome.Loading -> {}
+                is Outcome.Success -> {
+                    postSideEffect(MainSideEffect.GoToHomeFragment)
+                }
+
+                is Outcome.Failure -> {}
             }
         }
+        delay(hideSplashScreenDelay)
         postEvent(MainEvent.HideSplashScreen)
     }
 
     fun isDestinationInBottomNavigationBarInitialFragment(
         destinationId: Int,
-        bottomNavigationBarInitialFragmentIds: List<Int>
+        bottomNavigationBarInitialFragmentIds: List<Int>,
     ) {
-        if (destinationId in bottomNavigationBarInitialFragmentIds) postEvent(MainEvent.ShowBottomNavigationBar)
-        else postEvent(MainEvent.HideBottomNavigationBar)
+        if (destinationId in bottomNavigationBarInitialFragmentIds) {
+            postEvent(MainEvent.ShowBottomNavigationBar)
+        } else {
+            postEvent(MainEvent.HideBottomNavigationBar)
+        }
     }
 
     override fun reduceState(current: MainViewState, event: MainEvent): MainViewState =
