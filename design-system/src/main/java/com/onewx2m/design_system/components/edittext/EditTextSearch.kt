92748571563fbf41ec86949a95f7f51db7e3db0f
jinukeu <koownij@kakao.com>
2023-07-10 16:29:38 +0900

    feat: EditTextSearch 컴포넌트 추가

---
+++ design-system/src/main/java/com/onewx2m/design_system/components/edittext/EditTextSearch.kt
@@ -0,0 +1,59 @@
+package com.onewx2m.design_system.components.edittext
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.onewx2m.design_system.R
+import com.onewx2m.design_system.databinding.EditTextSearchBinding
+
+class EditTextSearch @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val binding: EditTextSearchBinding
+
+    val editText
+        get() = binding.editText
+
+    init {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        binding = EditTextSearchBinding.inflate(inflater, this, true)
+
+        binding.editText.setOnFocusChangeListener { _, hasFocus ->
+            if (isPreventLosingFocus) return@setOnFocusChangeListener
+            if (hasFocus) {
+                preventFocusClearedByAdjustResize()
+                binding.root.setBackgroundResource(R.drawable.bg_solid_empty_rounded_10_gray01)
+            } else {
+                binding.root.setBackgroundResource(R.drawable.bg_solid_empty_rounded_10_gray05)
+            }
+        }
+
+        val typedArray =
+            context.obtainStyledAttributes(attrs, R.styleable.EditTextSearch, defStyleAttr, 0)
+
+        val hint = typedArray.getText(R.styleable.EditTextSearch_editTextSearchHint)
+
+        binding.editText.hint = hint
+
+        typedArray.recycle()
+    }
+
+    private var isPreventLosingFocus = false
+
+    private fun preventFocusClearedByAdjustResize() {
+        isPreventLosingFocus = true
+        val losingFocusDelay = 250L
+
+        binding.editText.postDelayed({
+            binding.editText.run {
+                if (isFocused.not()) {
+                    requestFocus()
+                }
+            }
+            isPreventLosingFocus = false
+        }, losingFocusDelay)
+    }
+}
