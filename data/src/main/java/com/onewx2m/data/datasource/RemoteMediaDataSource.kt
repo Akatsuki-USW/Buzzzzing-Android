a77ac1612aa8fe4c53a89e6edb7dd8b6eb6d1c7e
jinukeu <koownij@kakao.com>
2023-07-12 16:27:40 +0900

    feat: RemoteMediaDataSource 추가

---
+++ data/src/main/java/com/onewx2m/data/datasource/RemoteMediaDataSource.kt
@@ -0,0 +1,16 @@
+package com.onewx2m.data.datasource
+
+import com.onewx2m.data.model.FileNameAndUrlEntity
+import com.onewx2m.domain.Outcome
+import com.onewx2m.domain.enums.S3Type
+import kotlinx.coroutines.flow.Flow
+import java.io.File
+
+interface RemoteMediaDataSource {
+
+    suspend fun changeImage(
+        s3Type: S3Type,
+        toDeleteUrls: String,
+        newFiles: List<File>,
+    ): Flow<Outcome<List<FileNameAndUrlEntity>>>
+}
