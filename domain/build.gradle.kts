662f37d366f1c4100e50753e82ee912f039a2281
jinukeu <koownij@kakao.com>
2023-07-13 16:51:50 +0900

    remove: DomainPlugin.kt

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {
-    id("buzzzzing.plugin.domain")
+    id("buzzzzing.plugin.java.library")
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.hilt.core)
 }
