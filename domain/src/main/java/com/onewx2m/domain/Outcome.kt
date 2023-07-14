834f557f2a545143168025ba3b6f72d9a52967e4
jinukeu <koownij@kakao.com>
2023-07-14 12:55:44 +0900

    feat: collectOutcome 메서드 추가

--- domain/src/main/java/com/onewx2m/domain/Outcome.kt
+++ domain/src/main/java/com/onewx2m/domain/Outcome.kt
@@ -1,6 +1,22 @@
 package com.onewx2m.domain
 
+import kotlinx.coroutines.flow.Flow
+
 sealed class Outcome<out T> {
     data class Success<T>(val data: T) : Outcome<T>()
     data class Failure<T>(val error: Throwable?) : Outcome<T>()
+}
+
+suspend fun <T> Flow<Outcome<T>>.collectOutcome(
+    beforeHandle: () -> Unit = {},
+    handleSuccess: (Outcome.Success<T>) -> Unit = {},
+    handleFail: (Outcome.Failure<T>) -> Unit = {},
+) {
+    collect { outcome ->
+        beforeHandle()
+        when (outcome) {
+            is Outcome.Success -> handleSuccess(outcome)
+            is Outcome.Failure -> handleFail(outcome)
+        }
+    }
 }
