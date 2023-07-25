b193e9d9a6ae92879b3d51dcc18b6e65bf2d0358
jinukeu <koownij@kakao.com>
2023-07-25 15:30:56 +0900

    chore: Parcelize 플러그인 추가

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -14,6 +14,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.android.library")
                 apply("org.jetbrains.kotlin.android")
+                apply("kotlin-parcelize")
             }
 
             extensions.configure<LibraryExtension> {
