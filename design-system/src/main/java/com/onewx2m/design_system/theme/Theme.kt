c2719df625b06aa40ebaaeb78b96d9db7048a110
jinukeu <koownij@kakao.com>
2023-09-24 17:22:17 +0900

    feat: 컴포즈용 Theme 추가

---
+++ design-system/src/main/java/com/onewx2m/design_system/theme/Theme.kt
@@ -0,0 +1,42 @@
+package com.onewx2m.design_system.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+@Composable
+fun BuzzzzingTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars =
+                !darkTheme
+        }
+    }
+
+    CompositionLocalProvider(
+        LocalTypography provides Typography,
+    ) {
+        MaterialTheme(
+            content = content,
+        )
+    }
+}
+
+object BuzzzzingTheme {
+    val typography: BuzzzzingTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTypography.current
+}
