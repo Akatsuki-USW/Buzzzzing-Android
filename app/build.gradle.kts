7043503894b7b18dc2699fca42176932cb2df7d4
jinukeu <koownij@kakao.com>
2023-11-04 00:46:25 +0900

    chore: versionCode 3 -> 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.onewx2m.buzzzzing"
         targetSdk = 33
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         buildConfigField(
